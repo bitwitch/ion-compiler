1e8f318549dab586785ed5ddd8f35bb1739a4dea
shaw <shawwian@gmail.com>
2023-06-08 12:21:20 -0400

    begin platform library for windowing, graphics, audio, etc.

---
+++ noir/build.bat
@@ -0,0 +1,57 @@
+@echo off
+if not exist build\ mkdir build
+pushd build
+
+setlocal EnableExtensions EnableDelayedExpansion
+set "_path=%~dp0"
+set SRC_DIR=%_path:~0,-1%
+set ION_DIR=%SRC_DIR%\..
+set URL=https://github.com/libsdl-org/SDL/releases/download/release-2.26.5/SDL2-devel-2.26.5-VC.zip
+set ZIPFILE="%CD%\SDL2-devel-2.26.5-VC.zip"
+
+REM copy SDL2 libraries to build directory if they haven't already been
+if not exist SDL2\ (
+	if exist %SRC_DIR%\SDL2\ (
+		xcopy /D /E /I %SRC_DIR%\SDL2\include SDL2\include\SDL2
+		echo F | xcopy /D %SRC_DIR%\SDL2\lib\x64\SDL2.lib SDL2\lib\SDL2.lib
+		echo F | xcopy /D %SRC_DIR%\SDL2\lib\x64\SDL2.dll .
+
+REM download SDL2 development libraries
+	) else (
+		bitsadmin /transfer downloadSDL2 /download %URL% %ZIPFILE%
+		if not exist %ZIPFILE% (
+			echo Build Error: Failed to download SDL2. 
+			echo You can manually download SDL2 development libraries for VC, extract the contents, and move them to a directory called SDL2 in the project root.
+			echo https://github.com/libsdl-org/SDL/releases/
+			endlocal
+			popd
+			exit /b 1
+		)
+
+REM unzip downloaded SDL2 zip file
+		powershell -nologo -noprofile -command "Expand-Archive -Path '%ZIPFILE%' -DestinationPath '%SRC_DIR%'"
+		if not exist %SRC_DIR%\SDL2-2.26.5 (	
+			echo "Failed to unzip %ZIPFILE%"
+			echo "You can manually extract the contents, and move them to a directory called SDL2 in the project root."
+			endlocal
+			popd
+			exit /b 1
+		)
+		move %SRC_DIR%\SDL2-2.26.5 %SRC_DIR%\SDL2
+		del %ZIPFILE%
+
+REM copy SDL2 libraries to build directory
+		xcopy /D /E /I %SRC_DIR%\SDL2\include SDL2\include\SDL2
+		echo F | xcopy /D %SRC_DIR%\SDL2\lib\x64\SDL2.lib SDL2\lib\SDL2.lib
+		echo F | xcopy /D %SRC_DIR%\SDL2\lib\x64\SDL2.dll .
+	)
+)
+
+REM compile noir.ion
+"%ION_DIR%\build\ion.exe" "%SRC_DIR%\noir.ion"
+
+REM compile main program
+cl /Zi /nologo /W3 /DSDL_MAIN_HANDLED /D_CRT_SECURE_NO_WARNINGS /ISDL2\include /Fesolitaire "%SRC_DIR%\noir_impl.c" /link /LIBPATH:SDL2\lib SDL2.lib
+
+endlocal
+popd
---
+++ noir/noir_impl.c
@@ -0,0 +1,77 @@
+#include "noir.c"
+
+#include <stdbool.h>
+#include <ctype.h>
+#include <SDL2/SDL.h>
+
+int sdl_scancode_from_noir_key[NUM_KEYS] = {
+    [KEY_RETURN]    = SDL_SCANCODE_RETURN,
+    [KEY_SPACE]     = SDL_SCANCODE_SPACE,
+    [KEY_BACKSPACE] = SDL_SCANCODE_BACKSPACE,
+    [KEY_TAB]       = SDL_SCANCODE_TAB,
+    [KEY_ESCAPE]    = SDL_SCANCODE_ESCAPE,
+    [KEY_LEFT]      = SDL_SCANCODE_LEFT,
+    [KEY_RIGHT]     = SDL_SCANCODE_RIGHT,
+    [KEY_UP]        = SDL_SCANCODE_UP,
+    [KEY_DOWN]      = SDL_SCANCODE_DOWN,
+    [KEY_LSHIFT]    = SDL_SCANCODE_LSHIFT,
+    [KEY_RSHIFT]    = SDL_SCANCODE_RSHIFT,
+    [KEY_LCTRL]     = SDL_SCANCODE_LCTRL,
+    [KEY_RCTRL]     = SDL_SCANCODE_RCTRL,
+    [KEY_LALT]      = SDL_SCANCODE_LALT,
+    [KEY_RALT]      = SDL_SCANCODE_RALT,
+};
+
+int noir_key_from_sdl_scancode[SDL_NUM_SCANCODES];
+
+bool init(void) {
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+		return false;
+    }
+
+	// init key tables
+	for (int c = 0; c < 256; ++c) {
+		// initialize ascii scancodes
+		if (isprint(c)) {
+			char str[] = {c, 0};
+			SDL_Scancode scancode = SDL_GetScancodeFromName(str);
+			if (scancode != SDL_SCANCODE_UNKNOWN) {
+				sdl_scancode_from_noir_key[(unsigned char)c] = scancode;
+			}
+		}
+	}
+	for (int key = 0; key < NUM_KEYS; key++) {
+		int scancode = sdl_scancode_from_noir_key[key];
+		if (scancode) {
+			noir_key_from_sdl_scancode[scancode] = key;
+		}
+	}
+
+	return true;
+}
+
+Window create_window(char *title, Int2 pos, Int2 size) {
+	Window window = {0};
+	int x = pos.x < 0 ? SDL_WINDOWPOS_CENTERED : pos.x;
+	int y = pos.y < 0 ? SDL_WINDOWPOS_CENTERED : pos.y;
+    window.sdl_window = SDL_CreateWindow(title, x, y, size.x, size.y, 0);
+	return window;
+}
+
+void update(void) {
+	SDL_PumpEvents();
+
+	const uint8_t *sdl_key_state = SDL_GetKeyboardState(NULL);
+	for (int i=0; i<NUM_KEYS; ++i) {
+		bool is_down = sdl_key_state[i];
+		int k = noir_key_from_sdl_scancode[i];
+		bool was_down = keys[k].down;
+		keys[k].down = is_down;
+		keys[k].pressed = is_down && !was_down;
+		keys[k].released = !is_down && was_down;
+	}
+}
+
+void destroy_window(Window window) {
+	SDL_DestroyWindow(window.sdl_window);
+}
